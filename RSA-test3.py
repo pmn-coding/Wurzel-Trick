58b14d6ec986790ea2cfad56d4575ae20dc8372f
PMN <54372892+pmn-coding@users.noreply.github.com>
2020-01-22 16:47:05 +0100

    Add files via upload

---
+++ RSA-test3.py
@@ -0,0 +1,31 @@
+def isprim(a):
+    x = 2
+    if a == 0 or a == 1: 
+        return False                   
+    elif a == 2: 
+        return True                    
+    else:
+        while x < a:                    
+            if a % x == 0: 
+                return False            
+            x += 1                      
+    return True
+
+n = int(input(">>> n: "))
+
+nn = n**0.5
+nn = int(nn)
+print(">>> gerundeter schitt:",nn)
+y = 2
+q = 0
+p = 0
+co = 1
+q = nn
+
+while not p * q == n:
+    p = n / q
+    if isprim(q):
+        break
+    else:
+        q += 1               
+print(q, p)
